6aea4bf3112df4f8d8a8652cc54b644d6a4345cd
Berkant Can Erkanat <berkantcanerkanat44@gmail.com>
2022-10-06 16:51:37 +0300

    baseURL changed

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const scenarioAPI = createApi({
   reducerPath: "scenarioAPI",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://scenario-back.herokuapp.com/" }),
   endpoints: (builder) => ({
     addScenario: builder.mutation({
       query: (payload) => ({
